b0c1a6da4dcdff5b1c02d6b465a9c095911c403d
razzaksr <razzaksr@gmail.com>
2020-09-18 19:59:41 +0530

    Introduction

---
+++ DLitheBatchTwo/src/main/java/dlithe/batchtwo/internship/DLitheBatchTwo/DLitheBatchTwoApplication.java
@@ -0,0 +1,13 @@
+package dlithe.batchtwo.internship.DLitheBatchTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DLitheBatchTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DLitheBatchTwoApplication.class, args);
+	}
+
+}
---
+++ DLitheBatchTwo/src/test/java/dlithe/batchtwo/internship/DLitheBatchTwo/DLitheBatchTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package dlithe.batchtwo.internship.DLitheBatchTwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DLitheBatchTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
